9f435bde87299c6cac337becb449dfcc6964e831
pavelberzkaln <p.berzkaln@gmail.com>
2022-05-22 22:12:30 +0300

    5.2.2. Реализовать Store<T extends Base> [#157]

--- src/main/java/ru/job4j/generics/MemStore.java
+++ src/main/java/ru/job4j/generics/MemStore.java
@@ -12,10 +12,7 @@ public final class MemStore<T extends Base> implements Store<T> {
      */
     @Override
     public void add(T model) {
-        T t = storage.get(model.getId());
-        if (t == null) {
-            storage.put(model.getId(), model);
-        }
+        storage.putIfAbsent(model.getId(), model);
     }
 
     /**метод выполняет замену объекта по id,
@@ -26,12 +23,12 @@ public final class MemStore<T extends Base> implements Store<T> {
      */
     @Override
     public boolean replace(String id, T model) {
-        if (storage.containsKey(id)) {
+        T t = findById(id);
+        boolean rsl = t != null;
+        if (rsl) {
             storage.put(id, model);
-            return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
     /**метод удаляет пару ключ-значение по id, который передается в метод
@@ -40,12 +37,12 @@ public final class MemStore<T extends Base> implements Store<T> {
      */
     @Override
     public boolean delete(String id) {
-        if (storage.containsKey(id)) {
+        T t = findById(id);
+        boolean rsl = t != null;
+        if (rsl) {
             storage.remove(id);
-            return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
     /**метод возвращает объект по ключу, который равен id,
@@ -55,10 +52,7 @@ public final class MemStore<T extends Base> implements Store<T> {
      */
     @Override
     public T findById(String id) {
-        if (storage.containsKey(id)) {
-            return storage.get(id);
-        } else {
-            return null;
-        }
+        T t = storage.get(id);
+        return t != null ? t : null;
     }
 }
