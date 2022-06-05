2f697045ecf559d61d593b4677a0a79a3c63b775
pavelberzkaln <p.berzkaln@gmail.com>
2022-06-05 12:00:52 +0300

    3. Переопределить только hashCode [#1003]

--- src/main/java/ru/job4j/collection/UserUsage.java
+++ src/main/java/ru/job4j/collection/UserUsage.java
@@ -8,10 +8,24 @@ import java.util.Map;
 public class UserUsage {
     public static void main(String[] args) {
         Map<User, Object> users = new HashMap<>();
-        User user1 = new User("Vasya", 1,
-                new GregorianCalendar(2018, Calendar.JANUARY, 5));
-        User user2 = new User("Vasya", 1,
-                new GregorianCalendar(2018, Calendar.JANUARY, 5));
+        Calendar calendar1 = new GregorianCalendar();
+        Calendar calendar2 = new GregorianCalendar();
+        calendar1.set(Calendar.YEAR, 2018);
+        calendar1.set(Calendar.MONTH, 1);
+        calendar1.set(Calendar.DAY_OF_MONTH, 5);
+        calendar1.set(Calendar.HOUR_OF_DAY, 22);
+        calendar1.set(Calendar.MINUTE, 10);
+        calendar1.set(Calendar.SECOND, 10);
+        calendar1.set(Calendar.MILLISECOND, 10);
+        calendar2.set(Calendar.YEAR, 2018);
+        calendar2.set(Calendar.MONTH, 1);
+        calendar2.set(Calendar.DAY_OF_MONTH, 5);
+        calendar2.set(Calendar.HOUR_OF_DAY, 22);
+        calendar2.set(Calendar.MINUTE, 10);
+        calendar2.set(Calendar.SECOND, 10);
+        calendar2.set(Calendar.MILLISECOND, 10);
+        User user1 = new User("Vasya", 1, calendar1);
+        User user2 = new User("Vasya", 1, calendar2);
         users.put(user1, new Object());
         users.put(user2, new Object());
         System.out.println(users.entrySet());
