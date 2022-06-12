498402e67d75666fceb729923a5975a5660a5557
pavelberzkaln <p.berzkaln@gmail.com>
2022-06-12 22:18:28 +0300

    0.1. FileOutputStream. [#252488]

--- src/main/java/ru/job4j/io/Matrix.java
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -1,11 +1,10 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
-import java.io.IOException;
 
 public class Matrix {
-    public static void main(String[] args) throws IOException {
-        FileOutputStream out = new FileOutputStream("result.txt");
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
         int[][] matrix = new int[5][5];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
@@ -13,6 +12,9 @@ public class Matrix {
                 out.write(String.valueOf(matrix[i][j]).getBytes());
                 out.write(System.lineSeparator().getBytes());
             }
+        }
+        } catch (Exception e) {
+                e.printStackTrace();
         }
     }
 }
