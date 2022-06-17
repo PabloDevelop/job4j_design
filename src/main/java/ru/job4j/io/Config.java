f9e120ac9508efe8c721636ef91b9d0a517bd77f
pavelberzkaln <p.berzkaln@gmail.com>
2022-06-17 21:43:59 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,59 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    /** должен считать все ключи в карту values.
+     * Важно в файле могут быть пустые строки
+     * и комментарии их нужно пропускать.
+     */
+    public void load() throws IllegalArgumentException {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            while (read.ready()) {
+                String line = read.readLine();
+                if (!line.startsWith("#") && line.length() > 0) {
+                    String[] splitlines = line.split("=", 2);
+                    if (splitlines.length != 2) {
+                        throw new IllegalArgumentException();
+                    }
+                    values.put(splitlines[0], splitlines[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("./io.data/app.properties"));
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,70 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./io.data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Petr Arsentev"));
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./io.data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
+    }
+
+    @Test
+    public void whenPairWithCommentAndEmptyString() {
+        String path = "./io.data/pair_with_comment_and_empty_string.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
+    }
+
+    @Test
+    public void whenPairWithTwoEquals() {
+        String path = "./io.data/pair_with_two_equals.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect="));
+        assertThat(config.value("name"), is("Petr Arsentev=I"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenExceptionWithoutValue() {
+        String path = "./io.data/key_without_pair.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenExceptionWithoutKey() {
+        String path = "./io.data/value_without_pair.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenExceptionWithoutKeyAndValues() {
+        String path = "./io.data/without_key_and_values.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenExceptionWithoutEquals() {
+        String path = "./io.data/without_equals.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+}
