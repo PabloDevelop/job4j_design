6cb1d7a802d4c39695b20bfadfe31b1fceb3c072
pavelberzkaln <p.berzkaln@gmail.com>
2022-06-24 00:08:12 +0300

    5.1. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,48 @@
+package ru.job4j.io;
+
+import java.util.*;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("The key doesn't exist.");
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        for (String s : args) {
+            List<String> parsedString = ArgsName.validate(s);
+                values.put(parsedString.get(0), parsedString.get(1));
+        }
+    }
+
+    public static List<String> validate(String args) {
+        List<String> splitLines = new ArrayList<>();
+        if (args.startsWith("-") && args.length() > 0 && args.contains("=")) {
+            splitLines.add(args.split("=", 2)[0].split("-", 2)[1]);
+            splitLines.add(args.split("=", 2)[1]);
+            if (splitLines.get(0).isEmpty() || splitLines.get(1).isEmpty()) {
+                throw new IllegalArgumentException("Wrong data.");
+            }
+        }
+        return splitLines;
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
