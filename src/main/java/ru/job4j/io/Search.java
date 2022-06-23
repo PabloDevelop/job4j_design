ef74513024f8344ed58360e7be3996122278c552
pavelberzkaln <p.berzkaln@gmail.com>
2022-06-23 22:17:45 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,6 +1,8 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
+import java.lang.annotation.Retention;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,14 +11,27 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("./io.data/");
-        search(start, p -> p.toFile().getName().endsWith(".log"))
-                .forEach(p -> System.out.println(p.getFileName()));
+        if (Search.validate(args)) {
+            Path start = Paths.get(args[0]);
+            search(start, p -> p.toFile().getName().endsWith(args[1]))
+                    .forEach(p -> System.out.println(p.getFileName()));
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static boolean validate(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        } else if (!args[0].startsWith("./")) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        } else if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("Wrong argument format. Try another one.");
+        }
+        return true;
     }
 }
