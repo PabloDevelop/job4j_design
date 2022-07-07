1b9a8ea7c822495303183291c4b27893b783449b
pavelberzkaln <p.berzkaln@gmail.com>
2022-07-07 16:32:41 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -13,5 +13,15 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+        int age = 33;
+        double growth = 1.82;
+        float footLength = 0.32f;
+        long hairCount = 1_000_000_000L;
+        byte fingersCount = 10;
+        boolean human = true;
+        char sex = 'M';
+        short salary = 30_000;
+        LOG.info("User info age : {}, foot length : {}, hair count : {}, fingers count : {}, race : {}, sex : {}, salary : {}", age, growth,
+                footLength, hairCount, fingersCount, human, sex, salary);
     }
 }
