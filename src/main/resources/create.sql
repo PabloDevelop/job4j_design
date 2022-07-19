f3a85baa70e2bdf936543edd420f27b282f6fb46
pavelberzkaln <p.berzkaln@gmail.com>
2022-07-20 00:39:23 +0300

    0. Что такое база данных. [#6860]

---
+++ src/main/resources/create.sql
@@ -0,0 +1,14 @@
+create table tasks(
+    id serial primary key,
+    title varchar(255),
+    task text,
+    deadline timestamptz
+)
+
+insert into tasks(title, task, deadline) values('Провести совещание',
+                                               'О космическом мусоре',
+                                               TIMESTAMP WITH TIME ZONE '2022-07-3 10:00:00+03');
+
+update tasks set deadline = TIMESTAMP WITH TIME ZONE '2022-07-30 16:00:00+03';
+
+delete from tasks;
