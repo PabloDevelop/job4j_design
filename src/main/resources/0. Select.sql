8a43b05fc779bfc764b8e4d79f528a8b63bbdab6
pavelberzkaln <p.berzkaln@gmail.com>
2022-07-21 21:26:47 +0300

    0. Select [#6861]

---
+++ src/main/resources/0. Select.sql
@@ -0,0 +1,15 @@
+create table fauna (
+    id serial primary key,
+    name text,
+    avg_age int,
+    discovery_date date
+);
+
+insert into fauna(name, avg_age, discovery_date) values ('line', 20, '08-Jan-1999');
+insert into fauna(name, avg_age, discovery_date) values ('fish', 1, '08-Jan-1700');
+insert into fauna(name, avg_age, discovery_date) values ('bird', 10, '08-Jan-1800');
+
+select * from fauna where name = 'fish';
+select * from fauna where avg_age < 21000 and avg_age > 10000;
+select * from fauna where discovery_date is null;
+select * from fauna where discovery_date < '01-Jan-1950';
