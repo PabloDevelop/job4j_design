f169da99a2aed751ee95897a19b6df391910df29
pavelberzkaln <p.berzkaln@gmail.com>
2022-07-21 23:15:26 +0300

    1. Inner join [#373617 #324650]

---
+++ src/main/resources/1. Inner join.sql
@@ -0,0 +1,18 @@
+create table konditerskaya(
+    id serial primary key,
+    title varchar(255)
+);
+
+create table goods(
+    id serial primary key,
+    title varchar(255),
+    konditerskaya_id int references konditerskaya(id)
+);
+
+insert into konditerskaya(title) values ('U Vasi');
+insert into konditerskaya(title) values ('U Leni');
+insert into goods(title, konditerskaya_id) values ('khleb', 1);
+insert into goods(title, konditerskaya_id) values ('bulochka', 2);
+
+select g.title Товар, k.title Название
+from goods g join konditerskaya k on g.konditerskaya_id = k.id;
