ae96d3becc9bcee030f3fa451727a5ac8a0cacfc
pavelberzkaln <p.berzkaln@gmail.com>
2022-07-25 17:35:09 +0300

    Хранилище машин [#1733 #325735]

--- src/main/resources/#1733.sql
+++ src/main/resources/#1733.sql
@@ -28,7 +28,12 @@ insert into car_transmissions(name) values ('Manual'), ('Manual'), ('Manual'), (
 insert into cars(name, body_id, engine_id, transmission_id) values ('Mersedess', 5, 3, 4), ('Opel', 2, 1, 1),
 ('BMW', 3, 4, 5), ('AUDI', 4, 6, 8);
 
-select * from cars;
+select c.name car_name, b.name body_name, e.name engine_name, t.name transmission_name
+from cars c
+left join car_bodies b on c.body_id = b.id
+left join car_engines e on c.engine_id = e.id
+left join car_transmissions t on c.transmission_id = t.id
+order by c.name;
 select b.name from car_bodies b left join cars c on c.body_id = b.id where c.id is null;
-select e.name from car_engines e left join cars c on c.body_id = e.id where c.id is null;
-select t.name from car_transmissions t left join cars c on c.body_id = t.id where c.id is null;
+select e.name from car_engines e left join cars c on c.engine_id = e.id where c.id is null;
+select t.name from car_transmissions t left join cars c on c.transmission_id = t.id where c.id is null;
