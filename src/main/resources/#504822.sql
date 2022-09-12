f419722082225f25f43c1ce4cae86e4010df458d
pavelberzkaln <p.berzkaln@gmail.com>
2022-09-12 21:53:13 +0300

    7. Нормализация БД [#504822 #342172]

--- src/main/resources/#504822.sql
+++ src/main/resources/#504822.sql
@@ -1,14 +1,15 @@
-CREATE TABLE person
+CREATE TABLE gender
 (
     id     SERIAL PRIMARY KEY,
-    name   varchar(255),
     gender varchar(8)
 );
 
-CREATE TABLE address
+CREATE TABLE person
 (
-    id      SERIAL PRIMARY KEY,
-    address text
+    id        SERIAL PRIMARY KEY,
+    name      varchar(255),
+    address   text,
+    gender_id int references gender (id)
 );
 
 CREATE TABLE movies
@@ -19,20 +20,19 @@ CREATE TABLE movies
 
 CREATE TABLE delivery
 (
-    id         SERIAL PRIMARY KEY,
-    movies_id  integer references movies (id),
-    person_id  integer references person (id),
-    address_id integer references address (id)
+    id        SERIAL PRIMARY KEY,
+    movies_id integer references movies (id),
+    person_id integer references person (id)
 );
 
-INSERT INTO person (name, gender)
-VALUES ('Ольга Егорова', 'женский'),
-       ('Иванов Сергей', 'мужской');
+INSERT INTO gender (gender)
+VALUES ('мужской'),
+       ('женский');
 
-INSERT INTO address (address)
-VALUES ('1-й Казанский перулок, д. 14'),
-       ('ул. Центральная, д. 40, кв. 74'),
-       ('ул. Ленина, д. 7, кв. 130');
+INSERT INTO person (name, address, gender_id)
+VALUES ('Ольга Егорова', '1-й Казанский перулок, д. 14', 1),
+       ('Иванов Сергей', 'ул. Центральная, д. 40, кв. 74', 1),
+       ('Иванов Сергей', 'ул. Ленина, д. 7, кв. 130', 2);
 
 INSERT INTO movies (name)
 VALUES ('Пираты Карибского моря'),
@@ -40,11 +40,12 @@ VALUES ('Пираты Карибского моря'),
        ('Человек, который изменил все'),
        ('Интерстеллар');
 
-INSERT INTO delivery (movies_id, person_id, address_id)
-VALUES (1, 1, 1),
-       (2, 1, 1),
-       (3, 2, 2),
-       (4, 2, 2),
-       (2, 2, 3);
+INSERT INTO delivery (person_id, movies_id)
+VALUES (1, 1),
+       (1, 2),
+       (2, 3),
+       (2, 4),
+       (3, 2);
 
-SELECT * from delivery;
+SELECT *
+from delivery;
