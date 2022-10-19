c9371edf7f7e2454fd8751e92040928cac73b7fc
pavelberzkaln <p.berzkaln@gmail.com>
2022-10-19 21:06:06 +0300

    2. Что такое TDD? [#4918 #352661]

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -64,7 +64,7 @@ public class CinemaTest {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
-        cinema.buy(account, 1, -1, date);
+        cinema.buy(account, 1, 1, date);
         assertThrows(IllegalArgumentException.class, () -> {
             cinema.buy(account, 1, 1, date);
         });
